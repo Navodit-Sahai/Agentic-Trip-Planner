d4efe2a3b28aec28df09692da0eb3340e3b027fa
Navodit-Sahai <sahainavodit781@gmail.com>
2025-10-19 23:47:55 +0530

    initial commit

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,48 @@
+
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConverterTool
+
+
+
+
+
+
+class GraphBuilder():
+    def __init__(self):
+        self.tools=[
+            WeatherInfoTool(),
+            PlaceSearchTool(),
+            CalculatorTool(),
+            CurrencyConverterTool()
+        ]
+        self.system_prompt=SYSTEM_PROMPT
+        self.llm_with_tools=ModelLoader().load_llm()
+
+
+    def agent_function(self,state:MessagesState):
+        """Main agent function"""
+        user_question=state["messages"]
+        input_question=[self.system_prompt]+user_question
+        response=self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
+
+
+    def build_graph(self):
+        graph_builder=StateGraph(MessagesState)
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_node("agent",self.agent_function)
+        graph_builder.add_node("tools",ToolNode(tools=self.tools))
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+
+
+    def __call__(self):
+        return self.build_graph()
---
+++ utils/model_loader.py
@@ -0,0 +1,39 @@
+from typing import Optional
+from pydantic import Field
+from langchain_groq import ChatGroq
+from utils.config_loader import load_config
+import os
+
+
+
+
+class ConfigLoader:
+    def __init__(self):
+        print(f"Loaded config.....")
+        self.config = load_config()
+    
+    def __getitem__(self, key):
+        return self.config[key]
+
+
+
+
+class ModelLoader:
+    model_provider="groq"
+    config: Optional[ConfigLoader]=Field(default=None,exclude=True)
+
+    def model_post_init(self, __context:any) -> None:
+        self.config=ConfigLoader()
+
+    class Config:
+        arbitrary_types_allowed=True
+
+    def load_llm(self):
+        """It loads and return the LLM Model"""
+        print("LLM loading...")
+        print("loading model from model provider: {self.model_provider}")
+        groq_api_key=os.getenv("GROQ_API_KEY")
+        model_name=self.config["llm"]["groq"]["model_name"]
+        llm=ChatGroq(model_name="llama-3.3-70b-versatile",api_key=groq_api_key)
+
+        return llm 
